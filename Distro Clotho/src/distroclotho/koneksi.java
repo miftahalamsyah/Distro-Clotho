c24bee4c17ab0788eee9bfc6cad9e0599963f248
Miftah Rizky Alamsyah <50770423+miftahalamsyah@users.noreply.github.com>
2022-06-16 22:47:11 +0700

    Add files via upload

---
+++ Distro Clotho/src/distroclotho/koneksi.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distroclotho;
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class koneksi {
+     private static java.sql.Connection koneksi;
+     
+      public static java.sql.Connection getKoneksi(){
+        if(koneksi == null){
+          try{
+              String url = "jdbc:mysql://localhost/distro";
+              String user = "root";
+              String password = "";
+              
+              DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+              
+              koneksi = DriverManager.getConnection(url, user, password);
+          }catch(SQLException t){
+              System.out.println("Error Membuat Koneksi");
+          }  
+        }
+        return koneksi;
+
+    }
+}
